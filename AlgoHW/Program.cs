067699d704f98f65e84b0d462818c9eb85e434ca
Dmytro Rudevych <mcatomicamc@gmail.com>
2020-01-12 19:34:33 +0200

    Init

---
+++ AlgoHW/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AlgoHW
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Enter N (0<N<110):");
+            int N = Convert.ToInt32(Console.ReadLine());
+            int num = Solve(N);
+            Console.WriteLine($"{N} - {num}");
+            Console.ReadKey();
+        }
+
+        private static int Parse(List<int> list)
+        {
+            string str = "";
+            foreach (int num in list)
+                str += num.ToString();
+            return Convert.ToInt32(str);
+        }
+
+        private static int Solve(int n)
+        {
+            List<int> list = new List<int>();
+            if (n < 10)
+                return n;
+            for (int index = 9; index >= 2; --index)
+            {
+                while (n % index == 0)
+                {
+                    n /= index;
+                    list.Add(index);
+                }
+            }
+            if (n > 10)
+                return -1;
+            list.Sort();
+            return Parse(list);
+        }
+    }
+}
---
+++ AlgoHW2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace AlgoHW2
+{
+    internal class Program
+    {
+        private static long[,] dp = new long[500, 500];
+
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Enter N (5<N<500):");
+            int N = Convert.ToInt32(Console.ReadLine());
+            dp[N, 0] = Solve(N, 0);
+            Console.WriteLine(dp[N, 0] - 1);
+            Console.ReadKey();
+        }
+
+        private static long Solve(int n, int last)
+        {
+            if (dp[n, last] > 0)
+            {
+                return Program.dp[n, last];
+            }
+            if (n == 0)
+            {
+                return 1;
+            }
+            int num = 1;
+            while (true)
+            {
+                if (n - num - last > last + num)
+                {
+                    dp[n, last] += Solve(n - num - last, last + num);
+                    ++num;
+                }
+                else
+                    break;
+            }
+            ++dp[n, last];
+            return dp[n, last];
+        }
+    }
+}
